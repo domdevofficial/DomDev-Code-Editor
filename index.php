064809ab7f20b48fb9aa3e1ede2839f56a0815bc
DomDev <domzkeeofficial@gmail.com>
2025-03-09 09:02:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Code Editor</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js"></script>
+    <script src="ace.js"></script>
 </head>
 <body>
 
